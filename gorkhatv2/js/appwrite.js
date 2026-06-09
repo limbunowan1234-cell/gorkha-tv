436441e2765fd6754a7c4e6712fc49b388cf5cb1
limbunowan1234-cell <limbunowan1234@gmail.com>
2026-06-09 23:47:30 +0530

    Update appwrite.js

--- gorkhatv2/js/appwrite.js
+++ gorkhatv2/js/appwrite.js
@@ -1,19 +1,18 @@
 import { Client, Databases, Storage, Account, Query } from "https://cdn.jsdelivr.net/npm/appwrite@14.0.1/+esm";
 
-const client = new Client();
-client
+const client = new Client()
   .setEndpoint("https://nyc.cloud.appwrite.io/v1")
   .setProject("6a280cbd0022eeb574a5");
 
 export const databases = new Databases(client);
 export const storage = new Storage(client);
 export const account = new Account(client);
+export const client_ = client;
 
 export const DB_ID = "6a280cde0009e6b2b556";
 export const COLLECTION_ID = "content";
-export const ARTISTS_COLLECTION_ID = "artists"; // ← REPLACE with your actual Collection ID
+export const ARTISTS_COLLECTION_ID = "artists";
 export const BUCKET_ID = "6a280d4100046ab86533";
-
 export const ADMIN_EMAIL = "Nowanad@gmail.com";
 
 export { Query };
